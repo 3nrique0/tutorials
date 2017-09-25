4310a3c8642d39b8fe9e2617df15d98b6cc76d5c
3nrique0 <3nrique.0@gmail.com>
2017-09-25 09:46:36 +0200

    Rearranged hierarchy of files

---
+++ linux_intro/play_ground/gc_content/calculate_GC_content.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+
+### CALCULATE GC CONTETNS OF A GENOME
+### Or for the lolz, of any text file :-P
+
+
+# Remove lines which are not relevant
+
+
+
+# Calculate the total number of effective lines
+
+
+
+# Count the number of c and C
+
+
+
+# Count the number of g and G
+
+
+
+## BONUS: Can you count c, C, g and G in one go ?
+
+
+
+# Now what ?
+# ((#C + #G)*100) (#chars - #lines)
+
+
+
---
+++ linux_intro/play_ground/scripts/intro_to_scripts.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+
+### WELCOME TO A BASH SCRIPT !
+
+# <- The hashtags are used to comment lines
+# <- Any text written after them won't be seen as code by the computer
+# <- All text after a hashtag, included, is named a COMMENT
+# <- Like how ??
+
+
+date > date.txt  # Like this
+
+
+
+
+### <- Do I need to put that many comment symbols ?
+
+# <- No, but it helps structure the script
+# Can I write more than one line ? -- Sure ! as far as you start with a 
+# Is this section going to be any useful ?? -- Nope, but it's a nice description
+
+
+
+
+### Shebang
+
+#  Also named sha-bang, hashbang, pound-bang, or hash-pling
+# It's a character composed of a # and a !
+# Both work toghether as one single character, it's an interpreter directive
+# It will tell the program loader with which language it should interpret the code
+
+
+
+
+### What is it for ?
+
+# Here you can write or copy paste the set of command lines which allow you
+# to achieve a particular task
+
+
+
+
+#### Why is it important ?
+
+# One word: REPETABILITY
+# This is the exact procedure to repeat the same task from the same dataset.
+# Isn't it cooler than trying to explain where did you clicked on excel ??
