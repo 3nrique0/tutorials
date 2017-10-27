7c1a26b37e74ba4226701cc7849f549853a6c26b
3nrique0 <3nrique.0@gmail.com>
2017-10-27 18:58:54 +0200

    added input file

--- linux_intro/playground/gc_content/calculate_GC_content_solution_v3.sh
+++ linux_intro/playground/gc_content/calculate_GC_content_solution_v3.sh
@@ -4,44 +4,36 @@
 ### CALCULATE GC CONTETNS OF A GENOME
 ### Or for the lolz, of any text file :-P
 
-# Set working directory
-#cd {path}/linux_intro/play_ground
+## This program will use only one argument: $1
 
-# Set file to be used as input
-#ln -s sgtpepper/sgtpepper input
-#ln -s ../../data/genome/GCF_000146045.2_R64_genomic.fna.gz input
-
-
-## We'll use zgrep on the first grep in case the input is zipped (.gz)
-## We'll use the command echo to give an indication of what the output on the screen is
 
 # Remove lines which are not relevant
 #zgrep "^>" -v input
 
 
 # Calculate the total number of effective lines 
-numLines=`zgrep "^>" -v input | wc -l`
+numLines=`zgrep "^>" -v $1 | wc -l`
 echo "Number of lines:	 ${numLines}"
 
 
 # Calculate the total number of effective characters
-numChars=`zgrep "^>" -v input | wc -c`
+numChars=`zgrep "^>" -v $1 | wc -c`
 echo "Number of characters:	 ${numChars}"
 
 
 
 # Count the number of c and C
-numC=`zgrep "^>" -v input | grep "c" -i -o | wc -l`
+numC=`zgrep "^>" -v $1 | grep "c" -i -o | wc -l`
 echo "Number of C:	${numC}"
 
 
 # Count the number of g and G
-numG=`zgrep "^>" -v input | grep "g" -i -o | wc -l`
+numG=`zgrep "^>" -v $1 | grep "g" -i -o | wc -l`
 echo "Number of G:	${numG}"
 
 
 ## BONUS: Can you count c, C, g and G in one go ?
-numGC=`zgrep "^>" -v input | grep "[gc]" -i -o | wc -l`
+numGC=`zgrep "^>" -v $1 | grep "[gc]" -i -o | wc -l`
 echo "Number of GC: ${numGC}"
 
 
