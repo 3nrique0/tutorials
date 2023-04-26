3a5c6cd22cbc099e1a82de5ec54df7d5c6435870
3nrique0 <3nrique.0@gmail.com>
2023-04-26 13:16:43 +0200

    Update launch_py.sh

--- SLURM/launch_py.sh
+++ SLURM/launch_py.sh
@@ -3,6 +3,10 @@
 #SBATCH -J python_test
 ##SBATCH -p workq
 #SBATCH --mem=8M
+##SBATCH --mail-user=name.lastname@inst.com
+##SBATHC --mail-type=BEGIN,END,FAIL
+##SBATCH -e error.txt
+##SBATCH -o output.txt
 
 python --version > python_version.txt
 
