7826ac671c325196aee09b4cc99e7283716fcf66
3nrique0 <3nrique.0@gmail.com>
2023-04-26 12:46:40 +0200

    Create myscript.py
    Generic basic python script with 3 different parts. It'll allow a waiting time for the execution in slurm

---
+++ SLURM/myscript.py
@@ -0,0 +1,31 @@
+## Python script
+
+## Coding styles are given in the pep8
+## https://peps.python.org/pep-0008/
+
+## Libraries to import
+
+import time
+import os
+import sys
+
+
+## First part of the script
+
+print("Hello ", os.getlogin(), "the current epoch (time in computers is:")
+
+print(time.time())
+
+
+## Second part of the script
+time.sleep(60)
+
+print("Now 60 seconds have passed since the last print")
+
+print(time.time())
+
+
+## Thrid part of the script 
+print("\nYour current version of python is")
+print(sys.version)
+print(sys.version_info)
