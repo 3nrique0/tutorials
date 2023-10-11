8a6b59815b044e206823e4d7521c2e17727955c2
3nrique0 <3nrique.0@gmail.com>
2023-10-12 00:16:24 +0200

    Added examples myscript.sh
    To the fourth part

--- SLURM/myscript.sh
+++ SLURM/myscript.sh
@@ -34,3 +34,14 @@ echo -e "\n Let's caracterize some variables!"
 echo -e "Download the data"
 wget https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv
 
+echo -e "\nThe amount of cars in this file is the number of lines minus the header:"
+echo "$(wc -l mtcars.csv | cut -d " " -f1 ) - 1" | bc
+
+echo -e "\nThis is the count for the occurrences of cylinder number (cyl)"
+cut -d , -f3 mtcars.csv | sort -n | uniq -c
+
+echo -e "\nThe 3 slowest times for the quarter of a mile (qsec) are:"
+cut -d , -f8 mtcars.csv | sort | head -3
+
+echo -e "\nThe number of cars depending on the number of gears they have"
+cut -d , -f11 mtcars.csv | sort -n | uniq -c | sed 's/  1 /num /'
