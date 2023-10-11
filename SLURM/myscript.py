2444b400af4a53bbe728b1e1dfb7be71d3df13c1
3nrique0 <3nrique.0@gmail.com>
2023-10-11 17:27:16 +0200

    Reorganized myscript.py
    made proprer 3rd and 4th parts

--- SLURM/myscript.py
+++ SLURM/myscript.py
@@ -13,6 +13,10 @@ import sys
 ## First part of the script
 print("Hello ", os.getlogin(), "the current epoch (time in computers is:")
 print(time.time())
+ 
+print("\nYour current version of python is")
+print(sys.version)
+print(sys.version_info)
 
 
 ## Second part of the script
@@ -21,21 +25,20 @@ print("Now 60 seconds have passed since the last print")
 print(time.time())
 
 
-## Thrid part of the script 
-print("\nYour current version of python is")
-print(sys.version)
-print(sys.version_info)
-
-
-# Fouth part
-# Let's make correlations!
-
-print("\nFourth part")
-print("Let's make correlations!\n")
+## Thrid part of the script
+print("\nThird part")
+print("Let's get data!\n")
 
 import pandas as pd
 
 mtcars = pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv')
+print(mtcars.head())
+
+
+# Fouth part
+# Let's make correlations!
+print("\nFourth part")
+print("Let's make correlations!\n")
 
 print("mpg~cyl\t",  mtcars.mpg.corr(mtcars.cyl))
 print("mpg~disp\t",  mtcars.mpg.corr(mtcars.disp))
