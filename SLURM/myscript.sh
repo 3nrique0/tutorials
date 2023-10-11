e50dd4e1adc8631720831be23851fee5ad39da72
3nrique0 <3nrique.0@gmail.com>
2023-10-11 17:33:38 +0200

    Reorganising myscript.sh
    Added 4 parts doing similar things that myscript.[py|R]
    The data characterization is not finished

--- SLURM/myscript.sh
+++ SLURM/myscript.sh
@@ -4,8 +4,33 @@
 #SBATCH -J myscript.sh
 #SBATCH -p workq
 
-echo "Hello $USER, it is:"
-date
+## GENERAL COMMENTS
+## This .sh script contains some parameters for SLURM
+## This script works just as well on a bash interface
+## The reason being that bash is the default shell in both:
+## in the server and in the nodes of the cluster
+
+# First part of the script
+echo "Hello $USER"
+echo "I'm $HOST. Welcome to you."
+echo "The date and time are: $(date)"
+
+
+# Second part of the script
 sleep 60
 echo "Sixty seconds have passed since the last print"
 date
+
+
+# Third part of the script
+# Let's get some data! 
+echo -e "\nLet's get some data -- Only by stream"
+curl https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv | head
+
+# Fouth part
+# Let's characterize some variables!
+
+echo -e "\n Let's caracterize some variables!"
+echo -e "Download the data"
+wget https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv
+
